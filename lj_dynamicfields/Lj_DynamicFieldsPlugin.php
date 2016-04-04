c770e86ad2150135aa174c4e5bb78d4d0fb0c17b
Josh Angell <josh@supercooldesign.co.uk>
2016-04-04 10:12:31 +0100

    Fixing getTemplatesPath() / setTemplatesPath() deprecation errors and requiring Craft 2.6.2778+ as a result.

--- lj_dynamicfields/Lj_DynamicFieldsPlugin.php
+++ lj_dynamicfields/Lj_DynamicFieldsPlugin.php
@@ -10,7 +10,7 @@ class Lj_DynamicFieldsPlugin extends BasePlugin
 
     function getVersion()
     {
-        return '0.5';
+        return '0.6';
     }
 
     function getDeveloper()
@@ -21,5 +21,18 @@ class Lj_DynamicFieldsPlugin extends BasePlugin
     function getDeveloperUrl()
     {
         return 'http://lewisjenkins.co.uk';
+    }
+
+    /**
+     * Require Craft 2.6.2778
+     *
+     * @throws Exception
+     */
+    public function onBeforeInstall()
+    {
+      if (version_compare(craft()->getVersion().'.'.craft()->getBuild(), '2.6.2778', '<'))
+      {
+        throw new Exception('LJ Dynamic Fields 0.6+ requires Craft CMS 2.6.2778+ in order to run.');
+      }
     }
 }
--- lj_dynamicfields/fieldtypes/Lj_DynamicFields_CheckboxesFieldType.php
+++ lj_dynamicfields/fieldtypes/Lj_DynamicFields_CheckboxesFieldType.php
@@ -11,50 +11,50 @@ class Lj_DynamicFields_CheckboxesFieldType extends BaseFieldType
 
 	public function getInputHtml($name, $values)
 	{
-		$oldTemplatesPath = craft()->path->getTemplatesPath();
-		craft()->path->setTemplatesPath(craft()->path->getSiteTemplatesPath());
-		
+		$oldMode = craft()->templates->getTemplateMode();
+		craft()->templates->setTemplateMode(TemplateMode::Site);
+
 		$variables = array();
 		foreach (craft() -> globals -> getAllSets() as $globalSet) :
 			$variables[$globalSet->handle] = $globalSet;
 		endforeach;
-		
+
 		$variables['element'] = $this->element;
 		$variables['model'] = $this->model;
-		
+
 		$options = json_decode('[' . craft()->templates->renderString($this->getSettings()->json, $variables) . ']', true);
-		craft()->path->setTemplatesPath($oldTemplatesPath);
-		
+		craft()->templates->setTemplateMode($oldMode);
+
 		if ($this->isFresh()) :
-		
+
 		$values = array();
-		
+
 			foreach ($options as $option) :
 				if (!empty($option['default'])) :
 					$values[] = $option['value'];
 				endif;
 			endforeach;
 		endif;
-		
+
 		return craft()->templates->render('_includes/forms/checkboxGroup', array(
 			'name' => $name,
 			'values' => $values,
 			'options' => $options
 		));
 	}
-	
+
 	protected function defineSettings()
     {
         return array(
             'json' => array(AttributeType::String)
         );
     }
-	
+
 	public function defineContentAttribute()
     {
         return AttributeType::Mixed;
     }
-	
+
 	public function getSettingsHtml()
     {
         return craft()->templates->render('lj_dynamicfields/settings/checkboxes', array(
